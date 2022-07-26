2b29e54dc77e27b01d0e27bebdcd61794478a7ec
Victor Pereira <victor.cordeiro2@gmail.com>
2022-07-26 11:53:54 -0300

    Listing reminders

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { StyleSheet, Text } from "react-native";
+import { CheckBox } from "react-native-elements";
+import { View } from "react-native";
+
+interface ListItemProps {
+  time: string;
+  title: string;
+}
+
+export default function ListItem({ time, title }: ListItemProps) {
+  const [isSelected, setSelection] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.text}>{time}</Text>
+        <Text style={styles.text}>{title}</Text>
+      </View>
+      <View>
+        <CheckBox
+          checked={isSelected}
+          onPress={() => {
+            setSelection(!isSelected);
+          }}
+          checkedColor="#F96D00"
+          style={styles.checkBox}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginRight: 32,
+    marginLeft: 32,
+  },
+  content: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  text: {
+    fontFamily: "Lato_400Regular",
+    fontSize: 16,
+    paddingRight: 16,
+  },
+  checkBox: {
+    color: "blue",
+    backgroundColor: "green",
+  },
+});
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,39 @@ import { StyleSheet, Text, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import Header from "../components/Header";
+import ListItem from "../components/ListItem";
 
 export default function Home() {
   return (
     <View>
       <Header title="REMINDER" />
 
+      <View style={styles.dayContainer}>
+        <Text style={styles.day}>TER</Text>
+        <Text style={styles.day}>26/07</Text>
+      </View>
+
+      <View style={styles.listContainer}>
+        
+        <ListItem time="09:00" title="Vitamina D" />
+        <ListItem time="09:00" title="Vitamina D" />
+
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  dayContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 32,
+  },
+  day: {
+    fontFamily: "Lato_400Regular",
+    fontSize: 16,
+    color: "#393E46",
+  },
+  listContainer: {
+  }
+});
